a60ccf5ab14a9d4aeedd79dae4b60b9ecd046c3d
Adam Peterson <alpha.eta.pi@gmail.com>
2025-09-19 15:11:57 -0600

    Move styling to a site stylesheet.

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -4,16 +4,8 @@ interface HelloWorldProps {}
 
 export const HelloWorld: React.FC<HelloWorldProps> = () => {
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-      fontFamily: 'Arial, sans-serif',
-      fontSize: '2rem',
-      color: '#333'
-    }}>
-      <h1>Hello World!</h1>
+    <div className="hello-world-container">
+      <h1 className="hello-world-heading">Hello World!</h1>
     </div>
   );
 };
