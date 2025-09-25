1409688592d7fdad9af9f0e02897e9501d563521
Adam Peterson <alpha.eta.pi@gmail.com>
2025-09-24 19:01:41 -0600

    Change port from 3000 to 8080.

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
